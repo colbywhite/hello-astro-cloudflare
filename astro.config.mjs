9e420b087e9bb7160472ea109bfb42435bdf5c7a
Colby M. White <colbywhite@users.noreply.github.com>
2026-02-03 19:49:16 -0600

    refactor: rm preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
-import preact from '@astrojs/preact';
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact()],
+  integrations: [mdx()],
   adapter: cloudflare(),
 });
